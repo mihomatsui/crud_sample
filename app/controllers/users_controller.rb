408be81d52ad406cd663e69df7fc7f84b51d6ae6
mihomatsui <mihomatsui.24@gmail.com>
2021-02-11 21:00:06 +0900

    redirectを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,9 @@ class UsersController < ApplicationController
 
   def create
     User.create(user_params)
+    redirect_to action: :index
   end
+
 
   def edit
     @user = User.find(params[:id])
@@ -19,12 +21,16 @@ class UsersController < ApplicationController
   def update
     user = User.find(params[:id])
     user.update(user_params)
+    redirect_to action: :index
   end
+
 
   def destroy
     user = User.find(params[:id])
     user.destroy
+    redirect_to action: :index
   end
+
 
   private
   def user_params
